b262b4c46d0510b43411da28325acc9edc8e66d9
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2022-01-27 22:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
  required_version = ">=0.14.9"
 }
 provider "aws" {
- region = "us-east-1"
+ region = "ap-south-1"
 }
 resource "aws_instance" "app_server" {
  instance_type = "t2.micro"
